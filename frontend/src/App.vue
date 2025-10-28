6a02dba94b68aa1368a064f9d0eebd9ef39e34dd
danielpuertabernal <daniel.puerta0522@uco.net.co>
2025-10-28 15:23:35 -0500

    Se agrega funcion para que limpie el localstorage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,10 +2,20 @@
   <router-view />
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import { useAuthStore } from "./store/authStore";
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.validateAuth();
+});
+</script>
 
 <style>
 body {
   margin: 0;
 }
 </style>
+
